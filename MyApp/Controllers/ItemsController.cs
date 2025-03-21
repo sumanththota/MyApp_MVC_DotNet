ab05339b82f32c0e5e8c3bdfdf33f31cd6965829
sumaaanth <sumanththota7@gmail.com>
2025-03-21 15:06:04 -0400

    Initial commit

---
+++ MyApp/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers;
+
+public class ItemsController : Controller
+{
+    // GET
+    public IActionResult Overview()
+    {
+        var item = new Item() { Name = "keyboard" };
+        return View(item);
+    }
+}
---
+++ MyApp/Models/Item.cs
@@ -0,0 +1,10 @@
+namespace MyApp.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    //we want to get these items using /items/overview
+    //we need to create a controller named items and a method named overview
+}
